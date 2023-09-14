d6aebbbca8d0fe0f6a839daeaf2ad1e8427e4c4a
Nizuc <nizuc1999@gmail.com>
2023-09-14 12:15:25 +0300

    Заменил lock + Dictionary на ConcurrentDictionary. Добавил возможность чтения текста заданными частями и возможность завершения процесса по нажатию любой клавиши во время выполнения.

--- Program.cs
+++ Program.cs
@@ -1,65 +1,169 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Text;
 
 public static class Program
 {
-    private static Dictionary<string, int> tripletsCount = new Dictionary<string, int>();
-    private static object lockObject = new object();
+    private static ConcurrentDictionary<string, int> tripletsCount =
+        new ConcurrentDictionary<string, int>(Environment.ProcessorCount, 100);
+
+    private static bool exitLoop = false;
 
     public static void Main()
     {
+        if (!TryGetPath(out var filePath))
+           return;
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        FillTripletsDictionary(filePath);
+        PrintTop10Triplets();
+        stopwatch.Stop();
+
+        Console.WriteLine($"Время выполненния программы: {stopwatch.ElapsedMilliseconds}мс. ");
+    }
+
+    private static bool TryGetPath(out string filePath)
+    {
         Console.WriteLine("Введите путь к файлу:");
-        var filePath = Console.ReadLine();
+        filePath = Console.ReadLine();
 
         if (!File.Exists(filePath))
         {
-            Console.WriteLine("Такого файла нет");
-            return;
+            Console.WriteLine("Такого файла нет.");
+            return false;
         }
 
         if (filePath.Substring(filePath.Length - 4, 4) != ".txt")
         {
-            Console.WriteLine("Неверный формат файла, ужен файл с расширением \".txt\"");
-            return;
+            Console.WriteLine("Неверный формат файла, нужен файл с расширением \".txt\".");
+            return false;
         }
 
-        Stopwatch stopwatch = new Stopwatch();
-
-        stopwatch.Start();
-        CountTriplets(File.ReadAllText(filePath));
-        PrintTop10Triplets();
-        stopwatch.Stop();
-        
-        Console.WriteLine($"Время выполненния программы: {stopwatch.ElapsedMilliseconds}мс. ");
+        return true;
     }
 
-    private static void CountTriplets(string text)
+    private static void FillTripletsDictionary(string path)
     {
-        Parallel.For(0, text.Length - 2, i =>
+        var fileLength = new FileInfo(path).Length;
+        Console.WriteLine($"Файл весит {GetWeight(fileLength)}, желаете его читать по частям? Y/N");
+        switch (Console.ReadLine().ToUpper())
         {
-            string triplet = text.Substring(i, 3);
-
-            lock (lockObject)
-            {
-                if (tripletsCount.ContainsKey(triplet))
+            case "Y":
+                Console.WriteLine("Введите объем буфера в символах");
+                if (int.TryParse(Console.ReadLine(), out int bufferLengt))
                 {
-                    tripletsCount[triplet]++;
+                    if (bufferLengt < 2)
+                    {
+                        Console.WriteLine("Введите буфер большего размера.");
+                        return;
+                    }
+
+                    if (bufferLengt > fileLength)
+                    {
+                        Console.WriteLine("Размер буфера не может превышать размер файла.");
+                        return;
+                    }
+
+                    StartFilling(ChunksRead, path, bufferLengt);
                 }
                 else
-                {
+                    Console.WriteLine("Неверный размер, размер буфера не должен превышать размеры int.");
+
+                break;
+            case "N":
+                StartFilling(FullRead, path, 0);
+                break;
+            default:
+                Console.WriteLine("Неправильный символ");
+                break;
+        }
+    }
+    
+    private static void CountTriplets(string text)
+    {
+        Parallel.For(0, text.Length - 2, (i, state) =>
+        {
+            if (exitLoop)
+                state.Break();
+
+            var triplet = text.Substring(i, 3);
+            if (char.IsLetter(triplet[0]) && char.IsLetter(triplet[1]) && char.IsLetter(triplet[2]))
+            {
+                if (tripletsCount.ContainsKey(triplet))
+                    tripletsCount[triplet]++;
+                else
                     tripletsCount[triplet] = 1;
-                }
             }
         });
+    }
+
+    private static void StartFilling(Action<string, int> filler, string path, int bufferrLength)
+    {
+        Thread thread = new Thread(() => filler(path, bufferrLength));
+        thread.Start();
+
+        while (!exitLoop)
+        {
+            if (Console.KeyAvailable)
+            {
+                exitLoop = true;
+            }
+
+            Thread.Sleep(100);
+        }
+
+        thread.Join();
+    }
+
+    private static void ChunksRead(string path, int bufferrLength)
+    {
+        using (FileStream fs = File.OpenRead(path))
+        {
+            byte[] buffer = new byte[bufferrLength];
+            int bytesRead;
+
+            while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string textChunk = Encoding.Default.GetString(buffer, 0, bytesRead);
+                CountTriplets(textChunk);
+                if (exitLoop)
+                    break;
+            }
+        }
+
+        exitLoop = true;
+    }
+
+    private static void FullRead(string path, int dummy = 0)
+    {
+        CountTriplets(File.ReadAllText(path));
+        exitLoop = true;
     }
 
     private static void PrintTop10Triplets()
     {
         var topTriplets = tripletsCount.OrderByDescending(x => x.Value)
-            .Where(x => char.IsLetter(x.Key[0]) && char.IsLetter(x.Key[1]) && char.IsLetter(x.Key[2]))
             .Take(10);
+
         foreach (var triplet in topTriplets)
         {
             Console.WriteLine($"Триплет {triplet.Key} встречается {triplet.Value} раз");
         }
+    }
+
+    private static string GetWeight(long fileWeight)
+    {
+        var fileExtencion = new string[] { "Bytes", "KB", "MB", "GB", "TB" };
+        var number = (decimal)fileWeight;
+        var counter = 0;
+        while (number / 1024 > 1024)
+        {
+            number /= 1024;
+            ++counter;
+        }
+
+        return $"{number:N4}{fileExtencion[counter]}";
     }
 }
